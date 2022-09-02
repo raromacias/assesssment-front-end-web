50ea8b5fea4504a41f9f2f070965f89ccc85c7f5
Raro Macias <ggmacias@gmail.com>
2022-09-02 12:35:48 -0400

    edit js done

--- about.js
+++ about.js
@@ -5,10 +5,18 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Your form has been submitted successfully');
+}
+function handleMouseOver(evt) {
+	evt.preventDefault();
+	
+	alert('You are great');
 }
 
 
 let form = document.querySelector('#contact');
+let pic = document.querySelector("img")
+
 
 form.addEventListener('submit', handleSubmit);
+pic.addEventListener('mouseover', handleMouseOver)
